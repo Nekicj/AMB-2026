40cb523c1dca72aabc8cdae1f1f711b941003a51
Nekicj <tolegenovdavid54@gmail.com>
2026-06-23 17:30:51 +0500

    vercel + neon

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig, Pool } from "@neondatabase/serverless";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const db = globalForPrisma.prisma ?? new PrismaClient();
+function createPrismaClient() {
+    const connectionString = "postgresql://neondb_owner:npg_ke67dSwusvgL@ep-hidden-frog-atjnd7wc.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require";
+    
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaNeon(pool);
+    return new PrismaClient({ adapter });
+}
+
+export const db = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
